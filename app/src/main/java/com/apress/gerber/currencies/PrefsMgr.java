3bcd1a7bb30bcdca359dd88e60f7cdf6b79e006c
Yi Qin <yiqin.mems@gmail.com>
2015-05-06 19:28:37 -0500

    No complete button yet.

--- app/src/main/java/com/apress/gerber/currencies/PrefsMgr.java
+++ app/src/main/java/com/apress/gerber/currencies/PrefsMgr.java
@@ -1,10 +1,14 @@
 package com.apress.gerber.currencies;
 
+import android.content.SharedPreferences;
+
 /**
  * Created by yiqin on 5/6/15.
  */
 public class PrefsMgr {
 
-    
+    private static SharedPreferences sSharedPreferences;
+
+    //
 
 }
